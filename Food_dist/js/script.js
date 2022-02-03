4f7490baf40174ab787148ec2a1d2b3d097c557d
ZAU <alexey.zhabin@gmail.com>
2022-02-03 21:03:31 +0200

    XML to fetch API mod completed

--- Food_dist/js/script.js
+++ Food_dist/js/script.js
@@ -240,11 +240,6 @@ window.addEventListener('DOMContentLoaded', () => {
             // form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
 
-            const request = new XMLHttpRequest();
-            request.open('POST', 'server.php');
-            
-            request.setRequestHeader('Content-type', 'application/json'); //при передаче FormData НЕ используем
-
             const formData = new FormData(form);
 
             const object = {};
@@ -253,21 +248,24 @@ window.addEventListener('DOMContentLoaded', () => {
                 object[key] = value;
             });
 
-            const json = JSON.stringify(object);
-
-            // request.send(formData); //отправляем форму, которую передали аргументом в функцию
-            request.send(json);
-
-            request.addEventListener('load', ()=> {
-                if (request.status === 200 ) {
-                    console.log(request.response);
-                    showThanksModal(message.succes);
-                    form.reset();
-                    statusMessage.remove();
-                    
-                } else {
-                    showThanksModal(message.failure);
-                }
+            fetch('server.php', {
+                method: "POST",
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(object)
+            })
+            .then(data => data.text())
+            .then((data) => {
+                console.log(data);
+                showThanksModal(message.succes);
+                statusMessage.remove();
+            })
+            .catch(() => {
+                showThanksModal(message.failure);
+            })
+            .finally(() => {
+                form.reset();
             });
 
         });
